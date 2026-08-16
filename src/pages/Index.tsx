aecf1fd7870797221854ee0215bcc54c6f03848f
epotapushina-oss <e.potapushina@gmail.com>
2026-08-16 21:08:41 +0200

    r

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -161,7 +161,7 @@ const PhotoCarousel = ({ photos, altPrefix, contain = false }: { photos: string[
         {photos.map((photo, index) => (
             <div
               key={index}
-              className={`flex-shrink-0 h-80 overflow-hidden ${contain ? "w-[26rem]" : "w-72"}`}
+              className={`flex-shrink-0 overflow-hidden ${contain ? "h-64 w-[20rem]" : "h-80 w-72"}`}
             >
               <img
                 src={photo}
