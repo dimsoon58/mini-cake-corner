28111e82be8c76dab8c64ed4e370428515c20b23
epotapushina-oss <e.potapushina@gmail.com>
2026-08-20 13:41:30 +0200

    c

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -194,7 +194,7 @@ const Index = () => {
       <section className="relative text-primary-foreground overflow-hidden min-h-[88vh]">
         <video
           className="absolute inset-0 w-full h-full object-cover"
-          style={{ objectPosition: "center 55%" }}
+          style={{ objectPosition: "center 50%" }}
           src={heroVideo}
           poster={heroPoster}
           autoPlay
