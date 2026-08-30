4ff60d6d6d0ac42c05e722d08bfdc26cfae6f775
epotapushina-oss <e.potapushina@gmail.com>
2026-08-30 20:01:43 +0200

    c

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -17,7 +17,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between gap-2 overflow-hidden rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 [&>span]:min-w-0 [&>span]:flex-1 [&>span]:text-left [&>span]:truncate",
+      "flex h-10 w-full items-center justify-between gap-2 overflow-hidden rounded-md border border-input bg-background px-3 py-2 text-left text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:block [&>span]:min-w-0 [&>span]:flex-1 [&>span]:text-left [&>span]:truncate [&>span]:whitespace-nowrap [&>span_img]:hidden [&>span_div]:block [&>span_div]:w-auto",
       className,
     )}
     {...props}
