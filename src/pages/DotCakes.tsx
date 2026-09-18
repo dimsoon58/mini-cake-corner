3b391aea418449a706bb0710ccb49211ea5efbac
Elizabeth Potapushina <e.potapushina@gmail.com>
2026-09-18 11:35:19 +0200

    c

--- src/pages/DotCakes.tsx
+++ src/pages/DotCakes.tsx
@@ -454,7 +454,7 @@ const DotCakes = () => {
                   return (
                     <SelectItem key={flavor.id} value={flavor.id} itemText={label}>
                       <div className="flex items-start gap-2">
-                        <img src={flavor.image} alt={flavor.name} className="w-8 h-8 object-contain flex-shrink-0 mt-0.5" />
+                        <img src={flavor.image} alt={flavor.name} className="w-12 h-12 object-contain flex-shrink-0" />
                         <div>
                           <span>{t(flavor.name, (flavor as { nameFr?: string }).nameFr ?? flavor.name)}</span>
                           {(flavor.description || flavor.descriptionFr) && (
